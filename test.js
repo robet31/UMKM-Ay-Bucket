a7c7f48e0a3acfa4c96e3644fbf816049140ac87
robet31 <sopoh006@gmail.com>
2026-05-08 02:41:18 +0700

    fix: resolve ERR_MODULE_NOT_FOUND in Vercel Edge

--- test.js
+++ test.js
@@ -1,4 +1,1 @@
-import { neon } from '@neondatabase/serverless';
-const client = neon('postgresql://test:test@test/test');
-const sql = (strings, ...values) => client(strings, ...values);
-console.log(sql`SELECT 1`);
+import { getConfig } from './api/db';
